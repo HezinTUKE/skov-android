c1b8eca9108356b792a1a8e407fb2443e827aaea
HezinTUKE <vova.gezin@gmail.com>
2023-10-23 00:02:38 +0200

    navigation with params

---
+++ app/src/main/java/com/example/skov/item/View.kt
@@ -0,0 +1,24 @@
+package com.example.skov.item
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ItemView(
+    id : Int
+){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "ID : $id")
+
+    }
+}
