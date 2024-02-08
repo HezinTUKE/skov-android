1438db87a1c5d68e1184333eec40a355b9697b45
HezinTUKE <vova.gezin@gmail.com>
2024-02-08 01:47:47 +0100

    State

--- app/src/main/java/com/example/skov/MainActivity.kt
+++ app/src/main/java/com/example/skov/MainActivity.kt
@@ -5,9 +5,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.skov.navigation.NavigationView
 import com.example.skov.ui.theme.SKOVTheme
@@ -32,12 +50,57 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             SKOVTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    val navController = rememberNavController()
-                    
-                    NavigationView(controller = navController)
+                Scaffold (
+                    bottomBar = {
+                        BottomAppBar(
+                            actions = {
+                                IconButton(onClick = { /* do something */ }) {
+                                    Image(painter = painterResource(id = R.drawable.laptop),
+                                        modifier = Modifier.size(25.dp),
+                                        contentDescription = "Shop List")
+                                }
+                                IconButton(onClick = { /* do something */ }) {
+                                    Image(painter = painterResource(id = R.drawable.heart),
+                                        modifier = Modifier.size(25.dp),
+                                        contentDescription = "Liked Items")
+                                }
+                                IconButton(onClick = { /* do something */ }) {
+                                    Image(painter = painterResource(id = R.drawable.landlord),
+                                        modifier = Modifier.size(25.dp),
+                                        contentDescription = "Own Items")
+                                }
+                                IconButton(onClick = { /* do something */ }) {
+                                    Image(painter = painterResource(id = R.drawable.comments),
+                                        modifier = Modifier.size(25.dp),
+                                        contentDescription = "Messages")
+                                }
+                            },
+
+                            floatingActionButton = {
+                                FloatingActionButton(
+                                    onClick = { /* do something */ },
+                                    containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                                    elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                                ) {
+                                    Image(painter = painterResource(id = R.drawable.add),
+                                        modifier = Modifier.size(35.dp),
+                                        contentDescription = "Add")
+                                }
+                            }
+
+                        )
+                    }
+                ) {
+                    Surface(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it)
+                    ) {
+                        val navController = rememberNavController()
+
+                        NavigationView(controller = navController)
+                    }
                 }
+                // A surface container using the 'background' color from the theme
             }
         }
     }
