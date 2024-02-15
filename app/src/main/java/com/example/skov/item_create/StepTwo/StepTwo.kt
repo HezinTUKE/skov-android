b95963dedecf943759eb13a06a723491dd726b0d
HezinTUKE <vova.gezin@gmail.com>
2024-02-15 03:16:52 +0100

    Button fix

--- app/src/main/java/com/example/skov/item_create/StepTwo/StepTwo.kt
+++ app/src/main/java/com/example/skov/item_create/StepTwo/StepTwo.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.itemsIndexed
 import androidx.compose.foundation.pager.PagerState
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,9 +21,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.skov.item_create.CategoryViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -62,12 +65,26 @@ fun StepTwo(
 
                 content = {
                     itemsIndexed(categoryObserver.state!!.categorys) { _, categ ->
-
                         OutlinedButton(
-//                            modifier = Modifier.size(155.dp),
-                            onClick = {  }
+                            onClick = {
+                                category.value = categ.id
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = if(category.value == categ.id) {
+                                    Color.Black
+                                }else{
+                                    Color.White
+                                }
+                            )
                         ) {
-                            Text(text = categ.name)
+                            Text(
+                                text = categ.name,
+                                color = if(category.value == categ.id) {
+                                    Color.White
+                                }else{
+                                    Color.Black
+                                }
+                            )
                         }
                     }
                 }
