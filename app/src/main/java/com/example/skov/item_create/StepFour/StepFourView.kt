4d422778db2b879451a9b1da6eba3c512407eab4
HezinTUKE <vova.gezin@gmail.com>
2024-02-20 22:38:33 +0100

    Few changes

--- app/src/main/java/com/example/skov/item_create/StepFour/StepFourView.kt
+++ app/src/main/java/com/example/skov/item_create/StepFour/StepFourView.kt
@@ -1,11 +1,18 @@
 package com.example.skov.item_create.StepFour
 
 import android.net.Uri
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.staggeredgrid.LazyHorizontalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import coil.compose.AsyncImage
 import com.example.skov.utils.ImagePickerView
 
 @Composable
@@ -13,8 +20,29 @@ fun StepFourView(
     selectedImages : MutableState<ArrayList<Uri?>>
 ){
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ImagePickerView(selectedImages = selectedImages)
+        ImagePickerView(
+            selectedImages = selectedImages,
+            list = {
+                LazyHorizontalStaggeredGrid(
+                    rows = StaggeredGridCells.Fixed(3),
+                    content = {
+                        Log.d("ListURIView", selectedImages.value.size.toString())
+                        itemsIndexed(it){ _, uri ->
+                            AsyncImage(
+                                modifier = Modifier.fillMaxSize(),
+                                model = uri,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                )
+            }
+        )
+
+
     }
 }
--- app/src/main/java/com/example/skov/utils/ImagePicker.kt
+++ app/src/main/java/com/example/skov/utils/ImagePicker.kt
@@ -1,37 +1,55 @@
 package com.example.skov.utils
 
 import android.net.Uri
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
 
 @Composable
 fun ImagePickerView(
-    selectedImages : MutableState<ArrayList<Uri?>>
+    selectedImages : MutableState<ArrayList<Uri?>>,
+    list : @Composable() (ArrayList<Uri?>) -> Unit
 ){
-    val pickMediaLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickMultipleVisualMedia(10),
-        onResult = {listUri ->
-            if(listUri.isNotEmpty()) {
-                listUri.forEach { uri ->
-                    if (!selectedImages.value.contains(uri)) {
-                        selectedImages.value.add(uri)
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        val pickMediaLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.PickMultipleVisualMedia(10),
+            onResult = { listUri ->
+                if (listUri.isNotEmpty()) {
+
+                    listUri.forEach { uri ->
+                        if (!selectedImages.value.contains(uri)) {
+                            selectedImages.value.add(uri)
+                        }
                     }
+
+                    Log.d("ListURI", selectedImages.value.size.toString())
+
                 }
             }
-        }
-    )
-
-    OutlinedButton(onClick = { pickMediaLauncher.launch(
-        PickVisualMediaRequest(
-            ActivityResultContracts.PickVisualMedia.ImageOnly
         )
-    ) }) {
-        Text(text = "Select Images")
-    }
 
+        OutlinedButton(onClick = {
+            pickMediaLauncher.launch(
+                PickVisualMediaRequest(
+                    ActivityResultContracts.PickVisualMedia.ImageOnly
+                )
+            )
+        }) {
+            Text(text = "Select Images")
+        }
+
+        list(selectedImages.value)
+
+    }
 }
